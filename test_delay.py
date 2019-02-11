9a1c7763a5d2470e51eaf99e73f8c812ed3ea301
Shpyntov Yury <shpyntov@restream.rt.ru>
2019-02-11 11:56:11 +0300

    delay test update

--- test_delay.py
+++ test_delay.py
@@ -1,30 +1,21 @@
 from requests import get
 from hamcrest import assert_that, equal_to
+from random import randint
+import pytest
 
 BASE_URL = 'http://httpbin.org/delay/'
 
 
 class TestDelay:
-    def get_resp_time_by_delay(self, delay):
-        time = int(get(BASE_URL + str(delay)).elapsed.total_seconds())
+    def get_delay(self, requested_delay):
+        time = int(get(BASE_URL + str(requested_delay)).elapsed.total_seconds())
         return time
 
-    def check_delay(self, delay):
-        if delay <= 0:
-            assert_that(self.get_resp_time_by_delay(delay), equal_to(0))
-        elif 0 < delay <= 10:
-            assert_that(self.get_resp_time_by_delay(delay), equal_to(delay))
-        elif delay > 10:
-            assert_that(self.get_resp_time_by_delay(delay), equal_to(10))
-
-    def test_delay_minus10_sec(self):
-        self.check_delay(-10)
-
-    def test_delay_0_sec(self):
-        self.check_delay(0)
-
-    def test_delay_5_sec(self):
-        self.check_delay(5)
-
-    def test_delay_15_sec(self):
-        self.check_delay(15)
+    @pytest.mark.parametrize("requested_delay,expected_delay", [
+        (randint(-20, -1), 0),
+        (0, 0),
+        (5, 5),
+        (randint(11, 20), 10)
+    ])
+    def test_delay(self, requested_delay, expected_delay):
+        assert_that(self.get_delay(requested_delay), equal_to(expected_delay))
