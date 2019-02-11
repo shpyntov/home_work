ed2e2fff441361bd1df3f6f175a6e2cd492e06ce
Shpyntov Yury <shpyntov@restream.rt.ru>
2019-02-11 11:30:57 +0300

    status code test update

--- test_status_code.py
+++ test_status_code.py
@@ -1,16 +1,26 @@
-from requests import get
-from hamcrest import assert_that, equal_to, not_
+from requests import post
+from hamcrest import assert_that, equal_to
+import pytest
 
 BASE_URL = 'http://httpbin.org/status/'
 
 
 class TestStatusCode:
     def get_code(self, code):
-        code = str(get(BASE_URL + code).status_code)
+        code = str(post(BASE_URL + code).status_code)
         return code
 
-    def test_code_403(self):
-        assert_that(self.get_code('403'), equal_to('403'))
-
-    def test_code_123(self):
-        assert_that(self.get_code('123456'), not_(equal_to('123456')))
+    @pytest.mark.parametrize("test_input,expected", [
+        ("100", "100"),
+        ("200", "200"),
+        ("302", "302"),
+        ("400", "400"),
+        ("401", "401"),
+        ("403", "403"),
+        ("500", "500"),
+        ("503", "503"),
+        ("666", "400"),
+        ("123456", "400")
+    ])
+    def test_code(self, test_input, expected):
+        assert_that(self.get_code(test_input), equal_to(expected))
