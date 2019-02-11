7c2023f5be674cbdc7f1bd624e69751dc319530d
Shpyntov Yury <shpyntov@restream.rt.ru>
2019-02-11 11:10:55 +0300

    исправил тест по делеям

--- test_delay.py
+++ test_delay.py
@@ -1,27 +1,30 @@
 from requests import get
-from hamcrest import assert_that, less_than
+from hamcrest import assert_that, equal_to
 
 BASE_URL = 'http://httpbin.org/delay/'
-TOLERANCE_TIME = 0.5 # технологическая погрешность
 
 
 class TestDelay:
     def get_resp_time_by_delay(self, delay):
-        time = get(BASE_URL + str(delay)).elapsed.total_seconds()
+        time = int(get(BASE_URL + str(delay)).elapsed.total_seconds())
         return time
 
-    def test_delay_5_sec(self):
-        delay = 5
-        assert_that(self.get_resp_time_by_delay(delay), less_than(delay + TOLERANCE_TIME))
-
-    def test_delay_15_sec(self):
-        delay = 15
-        assert_that(self.get_resp_time_by_delay(delay), less_than(delay + TOLERANCE_TIME))
-
-    def test_delay_0_sec(self):
-        delay = 0
-        assert_that(self.get_resp_time_by_delay(delay), less_than(TOLERANCE_TIME))
+    def check_delay(self, delay):
+        if delay <= 0:
+            assert_that(self.get_resp_time_by_delay(delay), equal_to(0))
+        elif 0 < delay <= 10:
+            assert_that(self.get_resp_time_by_delay(delay), equal_to(delay))
+        elif delay > 10:
+            assert_that(self.get_resp_time_by_delay(delay), equal_to(10))
 
     def test_delay_minus10_sec(self):
-        delay = -10
-        assert_that(self.get_resp_time_by_delay(delay), less_than(TOLERANCE_TIME))
+        self.check_delay(-10)
+
+    def test_delay_0_sec(self):
+        self.check_delay(0)
+
+    def test_delay_5_sec(self):
+        self.check_delay(5)
+
+    def test_delay_15_sec(self):
+        self.check_delay(15)
